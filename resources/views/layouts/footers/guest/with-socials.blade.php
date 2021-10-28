bfba280085d42c84aa347504fe62b36026125598
osdeibi <osdeibi@gmail.com>
2021-10-28 00:54:56 -0500

    Initial commit

---
+++ resources/views/layouts/footers/guest/with-socials.blade.php
@@ -0,0 +1,61 @@
+  <footer class="footer pb-5">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-8 mb-4 mx-auto text-center">
+                  <a href="#" target="_blank"
+                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                      Company
+                  </a>
+                  <a href="#" target="_blank"
+                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                      About Us
+                  </a>
+                  <a href="#" target="_blank"
+                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                      Team
+                  </a>
+                  <a href="#" target="_blank"
+                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                      Products
+                  </a>
+                  <a href="#" target="_blank"
+                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                      Blog
+                  </a>
+                  <a href="#" target="_blank"
+                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                      Pricing
+                  </a>
+              </div>
+              <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+                  <a href="#" target="_blank" class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fab fa-twitter"></span>
+                  </a>
+                  <a href="#" target="_blank" class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fab fa-facebook"></span>
+                  </a>
+                  <a href="#" target="_blank"
+                      class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fab fa-instagram"></span>
+                  </a>
+                  <a href="#" target="_blank"
+                      class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fab fa-google-play"></span>
+                  </a>
+                  <a href="#" target="_blank" class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fab fa-app-store"></span>
+                  </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-8 mx-auto text-center mt-1">
+                  <p class="mb-0 text-secondary">
+                      Copyright © {{ now()->year }} Webapp por <a style="color: #252f40;" href="#"
+                          class="font-weight-bold ml-1" target="_blank">Fundación Juntos Se Puede</a> &amp; <a style="color: #252f40;"
+                          href="#" class="font-weight-bold ml-1" target="_blank">i.tecnologia.co</a>
+              </div>
+              </p>
+          </div>
+      </div>
+      </div>
+  </footer>
